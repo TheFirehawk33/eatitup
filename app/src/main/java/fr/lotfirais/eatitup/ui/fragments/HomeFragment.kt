a661d80d8d145581e148e762e04a22815be27a0e
Lotfi Raïs <46958722+TheFirehawk33@users.noreply.github.com>
2022-12-02 12:46:39 +0100

    Recipe to master (#7)
    * Creation of the view home with a random recipe
    
    * Structure modification, creation of new fragments, and styling
    
    * Edition of "no picture" images
    
    * Avancement partie splashscreen (non-terminée), ajout de logo
    
    * Rollback modifications splashscreen
    
    * Recipe fragment created, many modifications
    
    * Share button and other changes in recipe

--- app/src/main/java/fr/lotfirais/eatitup/ui/fragments/HomeFragment.kt
+++ app/src/main/java/fr/lotfirais/eatitup/ui/fragments/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
