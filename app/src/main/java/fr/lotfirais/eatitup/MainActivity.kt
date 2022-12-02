b42f9261199adc91db7727669b10c1ff45283a6d
Gabriel F <43410045+Badjeck@users.noreply.github.com>
2022-12-02 17:51:40 +0100

    Add favorite and like icon in the toolbar  (#12)

--- app/src/main/java/fr/lotfirais/eatitup/MainActivity.kt
+++ app/src/main/java/fr/lotfirais/eatitup/MainActivity.kt
@@ -3,21 +3,36 @@ package fr.lotfirais.eatitup
 import android.content.Context
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuProvider
+import androidx.fragment.app.Fragment
 import fr.lotfirais.eatitup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         hideKeyBoardOnTouchOn(binding.root)
+
+        addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                // Add menu items here
+                menuInflater.inflate(R.menu.menu_favorite_page, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                // Handle the menu selection
+                return true
+            }
+        })
     }
+
 
     /**
      * Hides keyboard on touch
@@ -33,3 +48,5 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
+
